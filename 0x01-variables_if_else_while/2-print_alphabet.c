2d3c157ec8718263d8afbc1b19966ec4b0343e3b
Oyebode_Adetunji <adetunji.oyebode6@gmail.com>
2022-09-11 01:59:54 +0100

    alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Description: print alphabets in lower case
+  *Return: Always zero
+  */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar ('\n');
+	return (0);
+}
