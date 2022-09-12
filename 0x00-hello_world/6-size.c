78a3c8be6d03ba4a2be6518484205421a022d997
Oyebode_Adetunji <adetunji.oyebode6@gmail.com>
2022-09-12 22:40:04 +0100

    commit size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,10 @@
  */
 int main(void)
 {
-	int i;
-	double d;
-	char c;
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
-			printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 			return (0);
 }
