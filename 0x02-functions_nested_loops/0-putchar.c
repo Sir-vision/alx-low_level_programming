dd2654624dc5d2e14a176e224697cf3f7fe78619
Oyebode_Adetunji <adetunji.oyebode6@gmail.com>
2022-09-13 22:12:31 +0100

    commit _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <unistd.h>
+#include "main.h"
 
 /**
   * main - Entry point
@@ -8,7 +8,9 @@
 */
 
 int main(void)
-}
+{
+
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
