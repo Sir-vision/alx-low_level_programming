46692c9c13c5d92e73c4e37581112a2f03e57f32
Oyebode_Adetunji <adetunji.oyebode6@gmail.com>
2022-09-22 23:00:58 +0100

    4-rev_arrays commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * reverse_array -> reversing an array
+  * @a: array a
+  * @n: an element of an array
+  */
+void reverse_array(int *a, int n)
+{
+	int i, aux;
+
+	for (i = 0, i < n / 2; i++)
+	{
+		aux = *a[i];
+		*a[i] = *a[n - 1 - i];
+		*a[n - 1 - i] = awx;
+	}
+}
