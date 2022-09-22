b021d854386c1481846b834a6aaddce8838f93dc
Oyebode_Adetunji <adetunji.oyebode6@gmail.com>
2022-09-22 08:26:53 +0100

    3-strcmp commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strcmp -> for comparing strings
+  * @s1: param1
+  * @s2: param2
+  * Return: integer
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (0);
+}
