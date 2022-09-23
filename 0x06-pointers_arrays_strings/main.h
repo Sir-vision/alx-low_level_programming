6b11fcdd4c4a9fca271d8c2227be28fe7a79f99a
Oyebode_Adetunji <adetunji.oyebode6@gmail.com>
2022-09-23 15:08:46 +0100

    commit changes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,11 +8,8 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *cap_string(char *);
-<<<<<<< HEAD
+char *cap_string(char *str);
 char *leet(char *s);
-char *rot13(char *);
-=======
->>>>>>> d89e7d3d99604163d21f497cbdc14e756a904059
+char *rot13(char *s);
 
 #endif
