12c7bcf020ec5c4da5df55e01d96b8783e61e923
Oyebode_Adetunji <adetunji.oyebode6@gmail.com>
2022-09-23 04:38:32 +0100

    7-leet.c commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,23 +5,25 @@
   * Return: returns a string
   */
 
-char *leet(char *x)
+char *leet(char *s)
 {
-	int a = 0, b, l = 5;
-	char tr[5] = {'A', 'E', 'O', 'T', 'L'}
-	char trw[5] = {'4', '3', '0', '7', '1'}
+	int a = 0, b = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'}
+	char n[5] = {'4', '3', '0', '7', '1'}
 
-	while (x[a])
+	while (s[a])
 	{
 		b = 0;
 
 		while (b < l)
 		{
-			if (x[a] == tr[b] || x[a] - 32 == tr[b])
-				x[a] = trw[b];
+			if (s[a] == r[b] || s[a] - 32 == r[b])
+			{
+				s[a] = n[b];
+			}
 			b++;
 		}
 		a++;
 	}
-	return (x);
+	return (s);
 }
