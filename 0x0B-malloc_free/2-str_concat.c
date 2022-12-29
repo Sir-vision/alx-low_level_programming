1a3895d8ac493ea18e151533c65f529b02e51451
Oyebode <adetunji.oyebode6@gmail.com>
2022-12-29 23:54:15 +0100

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -43,10 +43,15 @@ char *str_concat(char *s1, char *s2)
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < len_1; i++)
+	{
 		ptr[i] = s1[i];
-	for (j = 0; s2[j]; j++, i++)
+	}
+
+	for (j = 0; j < len_2; j++, i++)
+	{
 		ptr[i] = s2[j];
+	}
 	ptr[i] = '\0';
 	return (ptr);
 }
