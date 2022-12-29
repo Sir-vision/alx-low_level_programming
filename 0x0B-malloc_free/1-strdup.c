89eab5717a46b34966078e52abdbecf2dbacf533
Oyebode <adetunji.oyebode6@gmail.com>
2022-12-29 01:23:38 +0100

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,65 +3,33 @@
 #include <stdlib.h>
 
 /**
-  * _strlen - count array
-  * @s: array of elements
+  * _strdup - duplicate string
+  * @str: array of elements
   * Return: 0
   */
 
-int _strlen(char *s)
+char *_strdup(char *str)
 {
-	unsigned int i;
+	char *ptr;
+	int len, i;
 
-	i = 0;
-	while (s[i] != '\0') /* count characters of string */
-	{
-		i++;
-	}
-	return (i);
-
-	/**
-	  * _strcpy - copy arrays
-	  *src: array of elements
-	  * @dest: dest array
-	  * Return: dest
-	  */
-
-	char *_strcpy(char *dest, char *src)
-	{
-		int i = 0;
-
-		while (src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
-
-		return (dest);
-	}
-
-	/**
-	  * _strdup - array of point of string
-	  * @ str: array of element
-	  * Return: pointer
-	  */
-
-	char *_strdup(chr *str)
-	{
-		char *dst;
-	unsigned int size;
-
-	if (str == 0)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	size = _strlen(str) + 1;
-	dst = (char *) malloc(size *sizeof(char));
-	if (dst == 0)
+	for (len = 0; str[len]; len++)
+		;
+
+	ptr = malloc(sizeof(char) * (len + 1));
+
+	if (!ptr)
 	{
 		return (NULL);
 	}
-		_strcpy(dst, str);
-		return (dst);
+	for (i = 0; i < len; i++)
+	{
+		ptr[i] = str[i];
 	}
+	ptr[i] = '\0';
+	return (ptr);
 }
